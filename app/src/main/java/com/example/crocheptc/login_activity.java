6b46d7ea34ddfdd816226743b172b8ec8d24e421
Bruno César <121142325+BrunoTiptc@users.noreply.github.com>
2024-01-17 19:04:28 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/crocheptc/login_activity.java
@@ -0,0 +1,16 @@
+package com.example.crocheptc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
