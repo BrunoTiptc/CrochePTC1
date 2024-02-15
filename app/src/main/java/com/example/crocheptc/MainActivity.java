b46097f124187bd1aa04023de40d91d4cd71c13f
Bruno César <121142325+BrunoTiptc@users.noreply.github.com>
2024-02-15 00:59:35 -0300

    ta tudo organizado agora

--- app/src/main/java/com/example/crocheptc/MainActivity.java
+++ app/src/main/java/com/example/crocheptc/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.crocheptc;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+    }
     public void abrir_tela_sobre(View v){
         Intent intent = new Intent(this, tela_sobre.class);
         startActivity(intent);
 
 
+
     }
+
+    public void abrir_login(View v){
+        Intent intent1 = new Intent(this, login_activity.class);
+        startActivity(intent1);
+
+    }
+
+
 }
